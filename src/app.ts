6b246ba2fef1f4160ef44b64116f2dc3f720c796
Your Name <you@example.com>
2022-08-30 19:01:33 +0300

    socket added

--- src/app.ts
+++ src/app.ts
@@ -7,33 +7,32 @@ import flash from "express-flash";
 import path from "path";
 import mongoose from "mongoose";
 import bluebird from "bluebird";
-import {MONGODB_URI} from "./util/secrets";
-import {Server} from "socket.io";
-import {createServer} from "http";
+import { MONGODB_URI } from "./util/secrets";
+import { Server } from "socket.io";
+import { createServer } from "http";
 import PositionsService from "./services/positions.service";
 import SocketService from "./services/socket.service";
 import Sender from "./services/sender.service";
+import cors from 'cors'
+import positionsController from './controllers/positionsController';
 
 // Create Express server
 const app = express();
+const server = require("http").createServer(app);
+const PORT: any = process.env.PORT || 4455;
 
-// Connect to MongoDB
+// MONGODB DETAILS
 const mongoUrl = "mongodb+srv://jovany:Jj12345@cluster0.4mfvt.mongodb.net/TradingData";
 mongoose.Promise = bluebird;
 
-mongoose.connect(mongoUrl, {useUnifiedTopology: true ,useNewUrlParser: true, useCreateIndex: true }).then(
-    () => {} 
-).catch(err => {
-    console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
-    // process.exit();
-});
-
 
 // הגדרות השרת
-app.set("port", process.env.PORT || 3006);
+
+// app.set("port", process.env.PORT || 4455);
 app.use(compression());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors()); // add this line
 
 app.use(session({
     secret: "Jovani123!$@#$",
@@ -54,17 +53,49 @@ app.use(session({
 
 // app.use(passport.initialize());
 // app.use(passport.session());
+
 app.use(flash());
 
 app.use(
-    express.static(path.join(__dirname, "public"), {maxAge: 31557600000})
+    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
 );
+
+
+app.use('/positions', positionsController);
+
+
+const init = async () => { //פונקצייה חכמה שמוודאת התחברות לדאטא בייס לפני הפעלת השרת
+    try {
+        const mongooseConnection = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        };
+        
+        await mongoose.connect(mongoUrl, mongooseConnection);
+        server.listen(PORT, () => { //הפעלת השרת
+            console.log('server is up on port' + PORT);
+        });
+
+    } catch (err) { //במידה והתחברות נכשלה
+        console.log(err);
+    };
+};
+
+init();
+
+
 
 //הגדרות socket.io
 const httpServer = createServer(app);
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 global.io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
 });
 
 // הוספת אירועים להתחברות והתנתקות לקוח
@@ -73,5 +104,5 @@ global.io = new Server(httpServer, {
 SocketService.addListenersToSocketAndUpdateTables(global.io);
 
 httpServer.listen(3007);
-PositionsService.listenToStockPositions();
+PositionsService.listenToPositions();
 export default app;
--- src/services/socket.service.ts
+++ src/services/socket.service.ts
@@ -2,12 +2,14 @@ import {Server, Socket} from "socket.io";
 import SocketModel, {SocketDocument} from "../models/socket";
 import UserInfoModel, {UserInfoDocument} from "../models/usersInfo";
 import {DefaultEventsMap} from "socket.io/dist/typed-events";
+import UserPositionsIB, {usersPositionsIBDocument} from '../models/usersPositionsIB.model'
 
 const addListenersToSocketAndUpdateTables = (io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap>) : void => {
     //אירוע התחברות לקוח
     io.on("connection", async (socket: Socket) => {
         const user = socket.handshake.auth?.user;
         const filter = {user: user};
+        console.log('user connected', socket.id);
         await SocketModel.findOneAndUpdate(filter, {
             id: socket.id,
             user: user
@@ -15,7 +17,6 @@ const addListenersToSocketAndUpdateTables = (io: Server<DefaultEventsMap, Defaul
             useFindAndModify: false,
             upsert: true
         });
-
         const userInfoFilter = {_id: user};
         await UserInfoModel.findOneAndUpdate(userInfoFilter, {
             gatewayStatus: true
@@ -23,6 +24,31 @@ const addListenersToSocketAndUpdateTables = (io: Server<DefaultEventsMap, Defaul
             useFindAndModify: false
         });
 
+        socket.on("onPositionClose", async (data) => {
+            const userPositionsIB = new UserPositionsIB(data);
+            await userPositionsIB.save()
+        });
+
+
+        socket.on("onPositionOpenFailure", (data) => {
+            console.log(data)
+        });
+
+        socket.on("onPositionCloseFailure", async (data) => {
+           const User = await SocketModel.findOne({user: data.user});
+           socket.to(User.id).emit("PositionCloseFailure", data)
+        });
+
+        socket.on("onCloseAllPositionFailure",async (data) => {
+            const User = await SocketModel.findOne({user: data.user});
+            socket.to(User.id).emit("CloseAllPositionFailure", data)
+        });
+
+        socket.on("onExtractPositionsDetails",async (data) => {
+            const User = await SocketModel.findOne({user: data.user});
+            socket.to(User.id).emit("ExtractPositionsDetails", data)
+        });
+        
         //אירוע התנתקות לקוח
         socket.on("disconnect", async () => {
             await SocketModel.deleteOne({
@@ -33,7 +59,7 @@ const addListenersToSocketAndUpdateTables = (io: Server<DefaultEventsMap, Defaul
             } as UserInfoDocument, {
                 useFindAndModify: false
             });
-        });
+        });       
     });
 };
 
